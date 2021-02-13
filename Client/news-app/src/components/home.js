e67520b2d4ae305afee76a175bba190f257c7133
wmordy <mwadii30@gmail.com>
2021-02-13 19:44:18 +0100

    few changes

--- Client/news-app/src/components/home.js
+++ Client/news-app/src/components/home.js
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import NewsComp from './newsComp'
 export default function Home() {
     return (
-        <div>
-             <h1>this is reading news page</h1>
+        <div className = "container-fluid d-flex justify-content-center py-2">
+            <div className = "row text-center">
+            <div className = "col-12 py-5">
+                <h1>this is reading news page</h1>
+                </div>
+             <NewsComp/>
+             <NewsComp/>
+             <NewsComp/>
+            </div>
+             
         </div>
     )
 }
---
+++ Client/news-app/src/components/newsComp.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function NewsComp() {
+    return (
+        <div className = "container-fluid">
+            <div className = "row ">
+                <div className = "col  text-center">
+                    <h1>title of the shit is must be okay </h1>
+                </div>
+                <div className = "col">
+                <h6><span class="badge badge-pill badge-dark">Sport</span></h6>
+
+                </div>
+                <div className = "col-12">
+                    <p>This is must be the description of the news</p>
+                    <img src="https://html5box.com/html5gallery/images/Wilderness_1024.jpg" class="img-fluid rounded mx-auto d-block" alt="Responsive image"></img>
+                   
+                </div>
+
+            </div>
+        </div>
+    )
+}
