b7b293d678ce752c55cb0c0cf02e380711dee6de
Ouggadi Mohammed Aymane <58845118+MohammedAymane@users.noreply.github.com>
2021-02-19 19:08:09 +0100

    Update news.js

--- Server/route/news.js
+++ Server/route/news.js
@@ -5,14 +5,13 @@ const fs = require("fs");
 
 
 
-router.post("/",async (req, res)=>{
+router.post("/", async (req, res) => {
     var imagename = Date.now();
     try {
         var image = req.files.image;
-        console.log(image.name.split(".")[1]);
-        imagename = imagename +"."+ image.name.split(".")[image.name.split(".").length-1];
+        imagename = imagename + "." + image.name.split(".")[image.name.split(".").length - 1];
         image.mv("./images/" + imagename, function (error, result) {
-          if (error) throw error;
+            if (error) throw error;
         });
         var parsedData = JSON.parse(req.body.data);
         const news = new News({
@@ -24,62 +23,62 @@ router.post("/",async (req, res)=>{
         });
         const savedNews = await news.save();
         res.send("News added successfully");
-    } catch(err){
-        console.log("Error while adding news : "+ err);
-        res.status().send("Error while adding news : "+ err);
+    } catch (err) {
+        console.log("Error while adding news : " + err);
+        res.status().send("Error while adding news : " + err);
     }
 })
 
 router.get("/", async (req, res) => {
-    let page = (req.query.page)-1;
-    if(!(req.query.page)){
+    let page = (req.query.page) - 1;
+    if (!(req.query.page)) {
         page = 0;
     }
     page = parseInt(page);
     let limit = req.query.limit;
-    if(!limit){
+    if (!limit) {
         limit = 10;
     }
     limit = parseInt(limit);
     let filter = req.query.filter;
     let sortby = req.query.sortby;
-    try{
+    try {
         var news
-        if(filter) {
-            news = await News.find({field: filter}).sort(sortby);
-            news = news.slice(page*limit,page*limit+limit);
-        }else{
+        if (filter) {
+            news = await News.find({ field: filter }).sort(sortby);
+            news = news.slice(page * limit, page * limit + limit);
+        } else {
             news = await News.find().sort(sortby);
-            news = news.slice(page*limit,page*limit+limit);
+            news = news.slice(page * limit, page * limit + limit);
         }
         res.send(news);
-    } catch(err) {
-        res.status(500).send("Error getting the news : "+err);
-        console.log("Error getting the news : "+err);
+    } catch (err) {
+        res.status(500).send("Error getting the news : " + err);
+        console.log("Error getting the news : " + err);
     }
 })
 
-router.delete("/:id", async (req, res) =>{
+router.delete("/:id", async (req, res) => {
     try {
         const imageLocation = await News.findOne({ _id: req.params.id });
         try {
-            fs.unlinkSync( imageLocation.photos);
+            fs.unlinkSync(imageLocation.photos);
             //Image removed
-          } catch (err) {
-            console.error("Error while removing the old image : "+err);
-          }
+        } catch (err) {
+            console.error("Error while removing the old image : " + err);
+        }
         const newsDeleted = await News.findByIdAndRemove({ _id: req.params.id });
         res.send("News removed successfuly");
-    } catch(err){
-        console.log("Error while removing the news : "+err);
-        res.send("Error while removing the news : "+err);
+    } catch (err) {
+        console.log("Error while removing the news : " + err);
+        res.send("Error while removing the news : " + err);
     }
 })
 
 
-router.put("/:id", async (req ,res) =>{
+router.put("/:id", async (req, res) => {
     var imagename = Date.now();
-    if(req.files == null){
+    if (req.files == null) {
         try {
             const oldnews = await News.findOne({ _id: req.params.id });
             var parsedData = JSON.parse(req.body.data);
@@ -90,122 +89,122 @@ router.put("/:id", async (req ,res) =>{
                 date: parsedData.date,
                 field: parsedData.field
             };
-    
-            const newsUpdated = await News.findOneAndUpdate({ _id: req.params.id },updatedNews)
+
+            const newsUpdated = await News.findOneAndUpdate({ _id: req.params.id }, updatedNews)
             res.send("News updated successfuly");
-        } catch(err){
-            res.send("Error while updating the news without updating image : "+err)
-            console.log("Error while updating the news updating image : "+err)
+        } catch (err) {
+            res.send("Error while updating the news without updating image : " + err)
+            console.log("Error while updating the news updating image : " + err)
         };
-    
-    } else{
 
-    // ssssssss
-    try{
-        const imageLocation = await News.findOne({ _id: req.params.id });
+    } else {
+
+        // ssssssss
         try {
-            fs.unlinkSync( imageLocation.photos);
-            //Image removed
-          } catch (err) {
-            console.error("Error while removing the old image : "+err);
-          }
+            const imageLocation = await News.findOne({ _id: req.params.id });
+            try {
+                fs.unlinkSync(imageLocation.photos);
+                //Image removed
+            } catch (err) {
+                console.error("Error while removing the old image : " + err);
+            }
 
-          var image = req.files.image;
-          imagename = imagename +"."+ image.name.split(".")[image.name.split(".").length-1];
-          image.mv("./images/" + imagename, function (error, result) {
-            if (error) throw error;
-          });
-          
-        var parsedData = JSON.parse(req.body.data);
-        const updatedNews = {
-            title: parsedData.title,
-            content: parsedData.content,
-            photos: "./images/" + imagename,
-            date: parsedData.date,
-            field: parsedData.field
+            var image = req.files.image;
+            imagename = imagename + "." + image.name.split(".")[image.name.split(".").length - 1];
+            image.mv("./images/" + imagename, function (error, result) {
+                if (error) throw error;
+            });
+
+            var parsedData = JSON.parse(req.body.data);
+            const updatedNews = {
+                title: parsedData.title,
+                content: parsedData.content,
+                photos: "./images/" + imagename,
+                date: parsedData.date,
+                field: parsedData.field
+            };
+
+            const newsUpdated = await News.findOneAndUpdate({ _id: req.params.id }, updatedNews)
+            res.send("News updated successfuly");
+        } catch (err) {
+            res.send("Error while updating the news with updating image: " + err)
+            console.log("Error while updating the news with updating image : " + err)
         };
-
-        const newsUpdated = await News.findOneAndUpdate({ _id: req.params.id },updatedNews)
-        res.send("News updated successfuly");
-    } catch(err){
-        res.send("Error while updating the news with updating image: "+err)
-        console.log("Error while updating the news with updating image : "+err)
-    };
     }
 })
 
 
 
-router.get("/:id", async (req, res) =>{
+router.get("/:id", async (req, res) => {
 
-        let page = (req.query.page)-1;
-        if(!(req.query.page)){
-            page = 0;
+    let page = (req.query.page) - 1;
+    if (!(req.query.page)) {
+        page = 0;
+    }
+    let limit = req.query.limit;
+    if (!limit) {
+        limit = 10;
+    }
+    let filter = req.query.filter;
+    let sortby = req.query.sortby;
+    var newsById;
+
+    if (req.params.id == "first") {
+
+        try {
+            var news
+            if (filter) {
+                news = await News.find({ field: filter }).sort(sortby);
+                news = news.slice(page * 10, page * 10 + limit);
+            } else {
+                news = await News.find().sort(sortby);
+                news = news.slice(page * 10, page * 10 + limit);
+            }
+            res.send(news[0]);
+        } catch (err) {
+            res.status(500).send("Error getting the first news : " + err);
+            console.log("Error getting the first news : " + err);
         }
-        let limit = req.query.limit;
-        if(!limit){
-            limit = 10;
+
+
+    } else if (req.params.id == "last") {
+
+        try {
+            var news
+            if (filter) {
+                news = await News.find({ field: filter }).sort(sortby);
+                news = news.slice(page * 10, page * 10 + limit);
+            } else {
+                news = await News.find().sort(sortby);
+                news = news.slice(page * 10, page * 10 + limit);
+            }
+            res.send(news[news.length - 1]);
+        } catch (err) {
+            res.status(500).send("Error getting the last news : " + err);
+            console.log("Error getting the last news : " + err);
         }
-        let filter = req.query.filter;
-        let sortby = req.query.sortby;
-        var newsById;
 
-        if(req.params.id=="first"){
+    } else if (req.params.id == "count") {
 
-            try{
-                var news
-                if(filter) {
-                    news = await News.find({field: filter}).sort(sortby);
-                    news = news.slice(page*10,page*10+limit);
-                }else{
-                    news = await News.find().sort(sortby);
-                    news = news.slice(page*10,page*10+limit);
-                }
-                res.send(news[0]);
-            } catch(err) {
-                res.status(500).send("Error getting the first news : "+err);
-                console.log("Error getting the first news : "+err);
-            }
+        try {
+            const news = await News.find();
+            res.send({ count: news.length });
+        } catch (err) {
+            res.status(500).send("Error getting the number of news : " + err);
+            console.log("Error getting the number of news :" + err);
+        }
 
+    } else {
 
-        }else if(req.params.id=="last"){
-
-            try{
-                var news
-                if(filter) {
-                    news = await News.find({field: filter}).sort(sortby);
-                    news = news.slice(page*10,page*10+limit);
-                }else{
-                    news = await News.find().sort(sortby);
-                    news = news.slice(page*10,page*10+limit);
-                }
-                res.send(news[news.length-1]);
-            } catch(err) {
-                res.status(500).send("Error getting the last news : "+err);
-                console.log("Error getting the last news : "+err);
-            }
-
-        }else if(req.params.id=="count"){
-
-            try{
-                const news = await News.find();
-                res.send({count:news.length});
-            } catch(err) {
-                res.status(500).send("Error getting the number of news : "+err);
-                console.log("Error getting the number of news :"+err);
-            }
-
-        }else{
-
-            try {
+        try {
             newsById = await News.findOne({ _id: req.params.id });
             res.send(newsById);
-        } catch(err){
-            console.log("Error while getting the news by id : "+err);
-            res.send("Error while getting the news by id : "+err);
+        } catch (err) {
+            console.log("Error while getting the news by id : " + err);
+            res.send("Error while getting the news by id : " + err);
         }
-        }
-    
+    }
+
 })
 
 
