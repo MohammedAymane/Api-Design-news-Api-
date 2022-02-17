7761ffbe332365e2274ba810aad3e3fd9521dcdb
Ouggadi Mohammed Aymane <58845118+MohammedAymane@users.noreply.github.com>
2022-02-17 16:24:22 +0100

    Refactor

--- Server/server.js
+++ Server/server.js
@@ -4,31 +4,29 @@ const cors = require("cors");
 const upload = require("express-fileupload");
 
 const corsOption = {
-    origin: "http://localhost:20699",
-  }
+  origin: "http://localhost:20699",
+};
 // Connect to the Database
 connectDB();
- 
-const app = express(); 
+
+const app = express();
 
 // Init Midleware
 app.use(cors(corsOption));
-app.use(express.json({extended:false}));
+app.use(express.json({ extended: false }));
 app.use(upload()); // permet l'upload des images
 
+app.get("/", (req, res) => {
+  res.send("API running");
+});
 
-app.get("/",(req, res) =>{ 
-    res.send("API running")
-})
-
-app.use('/images',express.static("./images"));
+app.use("/images", express.static("./images"));
 
 // Define Routes
-app.use('/news', require("./route/news"));
-
+app.use("/news", require("./routes/news"));
 
 const PORT = process.env.PORT || 9000;
 
-app.listen(PORT, ()=>{
-    console.log("Server started on port : "+PORT)
+app.listen(PORT, () => {
+  console.log("Server started on port : " + PORT);
 });
